6935b2847559b9ab22d394fa78d0854b46c44a40
Alexis Guérin <aguerin@student.42.fr>
2018-02-27 13:58:17 +0100

    Copie de l'environnement

--- src/sh.c
+++ src/sh.c
@@ -2,5 +2,14 @@
 
 int	main(void)
 {
+	extern char	**environ;
+	char		**env;
+
+	if ((env = dupenv((const char**)environ, ag_strlendouble(environ))))
+	{
+		ft_putendl("ok");
+	}
+	else
+		ft_putendl("pas ok");
 	return (0);
 }
