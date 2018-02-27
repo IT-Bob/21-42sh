3d28e82ad1bc8c43ec9f95388c615bdb0b34cec1
mtacnet <mtacnet@student.42.fr>
2018-02-27 14:47:27 +0000

    Creation de l'environnement
    - Si l'environnement n'existe pas il est créé sinon il est retourné.

--- src/sh.c
+++ src/sh.c
@@ -1,15 +1,49 @@
 #include "sh.h"
 
-int	main(void)
+static char		**set_env(void)
 {
-	extern char	**environ;
-	char		**env;
+	char	**env;
+	char	*tmp;
+	char	*tmp1;
 
-	if ((env = dupenv((const char**)environ, ag_strlendouble(environ))))
+	if ((env = (char**)ft_memalloc(sizeof(char*) * (2 + 1))))
 	{
-		ft_putendl("ok");
+		tmp = getcwd(NULL, 0);
+		tmp1 = ft_strjoin("PWD=", tmp);
+		if (!(env[0] = ft_strdup("SHLVL=1")) || !(env[1] = ft_strdup(tmp1)))
+			ag_strdeldouble(&env);
+		tmp ? ft_strdel(&tmp) : NULL;
+		tmp1 ? ft_strdel(&tmp1) : NULL;
+	}
+	return (env);
+}
+
+static char		**create_env(char **environ)
+{
+	char	**env;
+
+	env = NULL;
+	if (environ)
+	{
+		if (environ[0] == NULL)
+			env = set_env();
+		else
+			env = dupenv((const char**)environ, ag_strlendouble(environ));
+	}
+	return (env);
+}
+
+int				main(void)
+{
+	extern char		**environ;
+	char			**env;
+
+	if ((env = create_env(environ)))
+	{
+		ft_putendl_fd("env create", 2);
+		print_env((const char**)env);
 	}
 	else
-		ft_putendl("pas ok");
+		ft_putendl_fd("21sh: create environnement impossible.", 2);
 	return (0);
 }
