bec2c233d910fc37bb30cec86bd9f0e7e28cdba3
Alexis Guérin <aguerin@student.42.fr>
2018-04-26 14:56:36 +0200

    Correction de l'attente sur un `&` seul

--- src/quotes.c
+++ src/quotes.c
@@ -37,7 +37,12 @@ int			quotes(char **line, char c)
 		c = 0;
 	if (line && line[0])
 		while (line[0][++i])
+		{
 			c = is_that(&line[0][i], c);
+			if (c == '&' && (((i > 0 && line[0][i - 1] != '&') || i == 0) &&
+						(line[0][i + 1] != '&')))
+				c = 0;
+		}
 	else
 		return (-1);
 	return (c);
